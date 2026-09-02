df9690af2b223af33783bb7a942714ff346d6371
Matias Morán <83555127+matiasmoranc@users.noreply.github.com>
2026-09-02 17:18:23 -0300

    Configurar plantilla aislada para Firebase v2

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Pegá aquí la configuración de la NUEVA app web de Firebase.
+// No reutilices el proyecto de la versión anterior.
+export const firebaseConfig = {
+  apiKey: "PEGAR_API_KEY",
+  authDomain: "PEGAR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PEGAR_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "PEGAR_PROJECT_ID",
+  storageBucket: "PEGAR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "PEGAR_MESSAGING_SENDER_ID",
+  appId: "PEGAR_APP_ID"
+};
